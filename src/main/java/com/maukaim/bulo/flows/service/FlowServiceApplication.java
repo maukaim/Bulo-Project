e7446190bfda0cd561162c3cc9808e9a841fcc85
Julien Elkaim <julienelk@gmail.com>
2022-07-03 10:52:40 +0800

    First commit

---
+++ src/main/java/com/maukaim/bulo/flows/service/FlowServiceApplication.java
@@ -0,0 +1,12 @@
+package com.maukaim.bulo.flows.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowServiceApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(FlowServiceApplication.class, args);
+    }
+}
