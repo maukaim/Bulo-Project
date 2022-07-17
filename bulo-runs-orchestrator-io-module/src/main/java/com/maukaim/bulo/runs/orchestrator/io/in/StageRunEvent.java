7f91f7ebbe3ce89e34c104df913221ce8e300e53
Julien Elkaim <julienelk@gmail.com>
2022-07-17 18:12:36 +0800

    General structure refactoring

--- bulo-runs-orchestrator-io-module/src/main/java/com/maukaim/bulo/runs/orchestrator/io/in/StageRunEvent.java
+++
@@ -1,9 +0,0 @@
-package com.maukaim.bulo.runs.orchestrator.io.in;
-
-import com.maukaim.bulo.common.models.events.ExternalEvent;
-
-public interface StageRunEvent extends ExternalEvent {
-    StageRunEventType getEventType();
-
-    String getStageRunId();
-}
