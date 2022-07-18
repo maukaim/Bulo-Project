27dae90496d1368f7236eeff29da77077db7d5e9
Julien Elkaim <julienelk@gmail.com>
2022-07-18 10:04:56 +0800

    Adapted to new commons module

--- bulo-flows-app/src/main/java/com/maukaim/bulo/flows/app/TestController.java
+++ bulo-flows-app/src/main/java/com/maukaim/bulo/flows/app/TestController.java
@@ -1,7 +1,7 @@
 package com.maukaim.bulo.flows.app;
 
 
-import com.maukaim.bulo.commons.core.models.FlowStageId;
+import com.maukaim.bulo.commons.models.FlowStageId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
