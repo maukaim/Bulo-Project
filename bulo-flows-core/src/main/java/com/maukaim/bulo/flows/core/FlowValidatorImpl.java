498756b79f5b104a2bc1942da185393c7d8d6be7
Julien Elkaim <julienelk@gmail.com>
2022-08-23 17:33:33 +0800

    Typo

--- bulo-flows-core/src/main/java/com/maukaim/bulo/flows/core/FlowValidatorImpl.java
+++ bulo-flows-core/src/main/java/com/maukaim/bulo/flows/core/FlowValidatorImpl.java
@@ -1,13 +1,13 @@
 package com.maukaim.bulo.flows.core;
 
-import com.maukaim.bulo.commons.core.AcyclicValidator;
+import com.maukaim.bulo.commons.models.AcyclicValidator;
 import com.maukaim.bulo.flows.io.FlowData;
 
 public class FlowValidatorImpl implements FlowValidator {
 
     @Override
     public boolean validate(FlowData flowData) {
-        new AcyclicValidator(flowData.getAncestorsByStageId()).validate(); // Le FlowData devrait avoir une map de  ancestorSByStage
+        new AcyclicValidator(flowData.getAncestorsByStageId()).validate(); // Le FlowData devrait avoir une map de ancestorSByStage
         return true;
     }
 
