87d5080d04ddc8b75f1c95d5267ebe02cc8425ca
Julien Elkaim <julienelk@gmail.com>
2022-08-23 15:30:52 +0800

    Add main class and expected modules

---
+++ bulo-teams-app/src/main/java/com/maukaim/bulo/teams/app/TeamsMicroServiceApplication.java
@@ -0,0 +1,11 @@
+package com.maukaim.bulo.teams.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsMicroServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsMicroServiceApplication.class, args);
+    }
+}
