6ee865b4b13c9ab5814ec5f8360b8fd55f50d5ee
Julien Elkaim <julienelk@gmail.com>
2022-09-12 16:09:55 +0800

    Migrated app & data-lifecycle modules to main repo: Bulo-Project

--- bulo-triggers-scheduler/bulo-triggers-scheduler-data-lifecycle-module/src/main/java/com/maukaim/bulo/triggers/scheduler/data/lifecycle/ScheduleTriggerConfigAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.maukaim.bulo.triggers.scheduler.data.lifecycle;
-
-import com.maukaim.bulo.trigger.scheduler.io.ScheduleTriggerConfigDto;
-import com.maukaim.bulo.triggers.scheduler.data.ScheduleTriggerConfig;
-
-public interface ScheduleTriggerConfigAdapter {
-    ScheduleTriggerConfig adapte(ScheduleTriggerConfigDto dto);
-}
