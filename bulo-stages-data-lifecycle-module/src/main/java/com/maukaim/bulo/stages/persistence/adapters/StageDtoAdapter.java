069fb6fd96cb32e7a5c32680c557165aad74305f
Julien Elkaim <julienelk@gmail.com>
2022-09-12 12:33:48 +0800

    Migrated app & data-lifecycle to main repository, Bulo-Project

--- bulo-stages-data-lifecycle-module/src/main/java/com/maukaim/bulo/stages/persistence/adapters/StageDtoAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.maukaim.bulo.stages.persistence.adapters;
-
-import com.maukaim.bulo.stages.io.models.stages.StageDto;
-import com.maukaim.bulo.stages.models.stage.Stage;
-
-public interface StageDtoAdapter {
-    StageDto adapte(Stage stage);
-}
