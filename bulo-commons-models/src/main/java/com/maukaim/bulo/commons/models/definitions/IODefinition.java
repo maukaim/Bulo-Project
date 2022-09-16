b185593ad213e34daaf1fc53a4a60877c628c91a
Julien Elkaim <julienelk@gmail.com>
2022-09-16 22:10:15 +0800

    Modify signature method

--- bulo-commons-models/src/main/java/com/maukaim/bulo/commons/models/definitions/IODefinition.java
+++ bulo-commons-models/src/main/java/com/maukaim/bulo/commons/models/definitions/IODefinition.java
@@ -1,6 +1,6 @@
 package com.maukaim.bulo.commons.models.definitions;
 
 public interface IODefinition {
-    boolean canBeMultiple();
-    String getIOTypeId();
+    boolean isCanBeMultiple();
+    String getTypeId();
 }
