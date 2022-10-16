8c900895a85bb0b31fcc90190140f9ee868a75d0
Julien Elkaim <julienelk@gmail.com>
2022-10-16 22:42:56 +0800

    Moved commons-module to domain-modules

--- domain-modules/bulo-flows-module/bulo-flows-serialization-module/src/main/java/com/maukaim/bulo/flows/serialization/mixins/flow/IoDependencyDtoMixIn.java
+++ domain-modules/bulo-flows-module/bulo-flows-serialization-module/src/main/java/com/maukaim/bulo/flows/serialization/mixins/flow/IoDependencyDtoMixIn.java
@@ -2,9 +2,7 @@ package com.maukaim.bulo.flows.serialization.mixins.flow;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.maukaim.bulo.commons.models.FlowStageId;
 import com.maukaim.bulo.flows.io.flow.InputProviderDto;
-import com.maukaim.bulo.flows.io.flow.OwnerKeyTypeDto;
 
 import java.util.Set;
 
--- domain-modules/bulo-flows-module/bulo-flows-serialization-module/src/main/java/com/maukaim/bulo/flows/serialization/mixins/stage/StageUpdateEventMixIn.java
+++ domain-modules/bulo-flows-module/bulo-flows-serialization-module/src/main/java/com/maukaim/bulo/flows/serialization/mixins/stage/StageUpdateEventMixIn.java
@@ -3,8 +3,6 @@ package com.maukaim.bulo.flows.serialization.mixins.stage;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.maukaim.bulo.commons.io.StageUpdateEventType;
-import com.maukaim.bulo.flows.io.flow.FlowDto;
-import com.maukaim.bulo.flows.io.flow.FlowEventType;
 import com.maukaim.bulo.flows.io.stage.StageDto;
 
 import java.time.Instant;
