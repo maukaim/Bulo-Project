3cb8a6e1cc0b4be550cc6925383b8983e641185c
Julien Elkaim <julienelk@gmail.com>
2022-10-16 22:39:44 +0800

    Moved definitions module to domain-modules

--- microservices/bulo-definitions-microservice/bulo-definitions-app/src/main/java/com/maukaim/bulo/definitions/registry/app/beans/ServiceBeansConfig.java
+++ microservices/bulo-definitions-microservice/bulo-definitions-app/src/main/java/com/maukaim/bulo/definitions/registry/app/beans/ServiceBeansConfig.java
@@ -4,7 +4,6 @@ import com.maukaim.bulo.definitions.data.TechnicalStageDefinitionStore;
 import com.maukaim.bulo.definitions.registry.core.TechnicalStageDefinitionService;
 import com.maukaim.bulo.definitions.registry.core.TechnicalStageDefinitionServiceImpl;
 import com.maukaim.bulo.definitions.registry.core.TechnicalStageDefinitionValidator;
-import com.maukaim.bulo.definitions.io.TechnicalStageDefinitionEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
