6aaada4917d333a9be5bc0a22ac44906687595d7
Julien Elkaim <julienelk@gmail.com>
2022-10-16 22:32:14 +0800

    Moved stages module to domain-modules

--- app-commons/src/main/java/com/maukaim/bulo/app/commons/endpoints/StageController.java
+++ app-commons/src/main/java/com/maukaim/bulo/app/commons/endpoints/StageController.java
@@ -2,7 +2,6 @@ package com.maukaim.bulo.app.commons.endpoints;
 
 import com.maukaim.bulo.stages.io.events.CreateStageInstruction;
 import com.maukaim.bulo.stages.io.events.DeleteStageInstruction;
-import com.maukaim.bulo.stages.io.events.StageUpdateEvent;
 import com.maukaim.bulo.stages.io.models.stages.StageDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
