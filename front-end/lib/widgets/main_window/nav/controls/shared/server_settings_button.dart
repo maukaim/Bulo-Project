76abb99c924cd87fa29bf64e0bfa16c758896306
Julien Elkaim <julienelk@gmail.com>
2023-09-29 10:06:57 +0800

    Enhanced design of server settings button

--- front-end/lib/widgets/main_window/nav/controls/shared/server_settings_button.dart
+++ front-end/lib/widgets/main_window/nav/controls/shared/server_settings_button.dart
@@ -14,55 +14,58 @@ class ServerSettingsButton extends ConsumerWidget {
     final isCurrentServerConnected =
         ref.watch(isServerConnectedProvider(getCurrentServerConnector(ref)));
     return Flexible(
-      child: CustomButton(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-        borderRadius: BorderRadius.circular(4),
-        onPressed: () {
-          showDialog(
-            barrierColor: Colors.black12.withOpacity(0.2),
-            context: context,
-            builder: (BuildContext context) {
-              return SettingsDialog(dialogContext: context);
-            },
-          );
-          // Handle your click action here
-        },
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Icon(
-                size: 18,
-                currentServer == null
-                    ? Icons.public
-                    : currentServer.isEmbedded()
-                        ? CupertinoIcons.desktopcomputer
-                        : Icons.public,
-                color: isCurrentServerConnected.when(
-                  loading: () => Colors.grey,
-                  data: (data) {
-                    return data ? Colors.green : Colors.red;
-                  },
-                  error: (_, __) {
-                    print(
-                        "Issue when checking EmbeddedServer. Error itself: $_ \nStack Trace: $__");
-                    return Colors.orangeAccent;
-                  },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 4.0),
+        child: CustomButton(
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(18)),
+          onPressed: () {
+            showDialog(
+              barrierColor: Colors.black12.withOpacity(0.2),
+              context: context,
+              builder: (BuildContext context) {
+                return SettingsDialog(dialogContext: context);
+              },
+            );
+            // Handle your click action here
+          },
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Icon(
+                  size: 18,
+                  currentServer == null
+                      ? Icons.public
+                      : currentServer.isEmbedded()
+                          ? CupertinoIcons.desktopcomputer
+                          : Icons.public,
+                  color: isCurrentServerConnected.when(
+                    loading: () => Colors.grey,
+                    data: (data) {
+                      return data ? Colors.green : Colors.red;
+                    },
+                    error: (_, __) {
+                      print(
+                          "Issue when checking EmbeddedServer. Error itself: $_ \nStack Trace: $__");
+                      return Colors.orangeAccent;
+                    },
+                  ),
                 ),
               ),
-            ),
-            Flexible(
-              child: Text(
-                currentServer?.serverName ?? "Select server",
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blueGrey.shade300),
+              Flexible(
+                child: Text(
+                  currentServer?.serverName ?? "Select server",
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueGrey.shade300),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
